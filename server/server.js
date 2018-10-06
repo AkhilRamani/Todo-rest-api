3662c1bbd5ea9a6b1dfb0216f687a47f1d1ded77
Akhil <ramaniakhil094@gmail.com>
2018-10-06 22:20:10 +0530

    added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id',(req,res)=>{
     }).catch((e)=> res.status(400).send());
 });
 
+app.delete('/todos/:id', (req,res)=>{
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=> res.status(400).send());
+});
+
 app.listen(port,()=>{
     console.log(`Started ip at port ${port}`);
 });
