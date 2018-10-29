c815a359e7e527d1cdb4393383c7aa654069dd7a
Akhil <ramaniakhil094@gmail.com>
2018-10-29 15:25:28 +0530

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -84,6 +84,25 @@ UserSchema.statics.findByToken = function(token){
     });
 };
 
+UserSchema.statics.findByCredentials = function(email,password){
+    var User= this;
+
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject)=>{
+            bcrypt.compare(password, user.password, (err, res)=>{
+                if(res){
+                    resolve(user);
+                } else{
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 var User = mongoose.model('User',UserSchema);
 
 module.exports= {User};
