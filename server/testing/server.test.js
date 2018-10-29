f5ba4df1f2ec5b6b7f4ed0378b91e2b25e60fd21
Akhil <ramaniakhil094@gmail.com>
2018-10-29 16:39:21 +0530

    add POST /users/login test server.test.js

--- server/testing/server.test.js
+++ server/testing/server.test.js
@@ -211,7 +211,7 @@ describe('POST /users', ()=>{
                 expect(user).toBeTruthy();
                 expect(user.password).not.toBe(password);
                 done();
-            });
+            }).catch((e)=> done(e));
         });
     });
 
@@ -231,5 +231,50 @@ describe('POST /users', ()=>{
         .send({ email: users[0].email, password:'123456'})
         .expect(400)
         .end(done);
+    });
+});
+
+describe('POST /users/login', ()=>{
+    it('should login user and return token', (done)=>{
+
+        request(app)
+        .post('/users/login')
+        .send({email:users[1].email, password: users[1].password})
+        .expect(200)
+        .expect((res)=>{
+            expect(res.headers['x-auth']).toBeTruthy();
+        }).end((err, res)=>{
+            if(err){
+                return done();
+            }
+
+            User.findById(users[1]._id).then((user)=>{
+                expect(user.tokens[0]).toMatchObject({
+                    access: 'Auth',
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e)=> done(e));
+        });
+    });
+
+    it('should reject invalid login', (done)=>{
+
+        request(app)
+        .post('/users/login')
+        .send({email: users[1].email, password: 'wrong pass'})
+        .expect(400)
+        .expect((res)=>{
+            expect(res.headers['x-auth']).toBeFalsy();
+        }).end((err, res)=>{
+            if(err){
+                return done();
+            }
+
+            User.findById(users[1]._id).then((user)=>{
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e)=> done(e));
+        });
     });
 });
